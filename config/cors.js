62098771fcca54d292fe1c0ec541db885d2b3d9f
Pedro Jesus <pedrotiagojesus1995@gmail.com>
2024-11-20 23:40:46 +0000

    Update CORS

--- config/cors.js
+++ config/cors.js
@@ -3,7 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const corsOptions = {
-    origin: ["http://localhost:5173", process.env.API_URL],
+    origin: [
+        "http://localhost:5173",
+        "http://192.168.50.104:5173",
+        process.env.API_URL,
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
 };
